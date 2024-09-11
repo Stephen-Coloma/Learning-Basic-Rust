d3d31c38d5be3e2d71d3fd6f1d88bb9f115a3438
Sanchie Earl Guzman <2232886@slu.edu.ph>
2024-09-11 15:03:26 +0800

    Created the codes for ownership, shadowing, and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+mod borrowing {
+    pub mod borrowing;
+}
+
+
+
+mod shadowing {
+    pub mod shadowing;
+}
+
+mod ownership {
+    pub mod  ownership;
+}
+
 fn main() {
     println!("Hello, world!");
+    println!();
+
+    println!("Concept of Ownership");
+    println!("=================================");
+    ownership::ownership::main();
+    println!("=================================");
+    println!();
+
+    println!("Concept of Borrowing");
+    println!("=================================");
+    borrowing::borrowing::main();
+    println!("=================================");
+    println!();
+
+    println!("Concept of Shadowing");
+    println!("=================================");
+    shadowing::shadowing::main();
+    println!("=================================");
+    println!();
+
 }
---
+++ src/shadowing/shadowing.rs
@@ -0,0 +1,27 @@
+pub fn main(){
+    println!("Welcome to Shadowing");
+
+    let student = String::from("Sanchie");
+    println!("{}", student);
+
+    let student = student + " Earl";
+    println!("{}", student);
+
+    let student = student + " Guzman";
+    println!("{}", student);
+
+    //changing the data type
+    let student = false;
+    println!("{}",student);
+
+    // //reusing variables in different scopes
+    // let i = 3;
+    //
+    // {
+    //     let i = i * 5;
+    //     println!("The value of inner i is {}", i);
+    // }
+    //
+    // println!("The value of outer i is {}", i);
+
+}
