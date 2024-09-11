150018f4d7971e3d1e03834a46b3755f1eda6d9b
Leonhard Leung <Leonhardleung@gmail.com>
2024-09-11 18:21:53 +0800

    feat: Implement code for demonstrating Abstraction and Polymorphism

--- src/polymorphism_abstraction/abs_poly_test.rs
+++ src/polymorphism_abstraction/abs_poly_test.rs
@@ -1,72 +1,6 @@
-fn main() {
-    // instantiate logbook
-    let mut log_book = LogBook::new();
+use indexmap::IndexMap;
 
-    // creation of students
-    let undergraduate1 = Box::new(Undergraduate {
-        first_name: String::from("Stephen"),
-        last_name: String::from("Horseshoes"),
-        student_id: 2234532,
-        major: String::from("Computer Science"),
-        year: String::from("Junior"),
-        credit_hours: 19,
-        average_grade: 93.5,
-    });
-
-    let undergraduate2 = Box::new(Undergraduate {
-        first_name: String::from("Mario"),
-        last_name: String::from("Nonato"),
-        student_id: 2230683,
-        major: String::from("Computer Science"),
-        year: String::from("Junior"),
-        credit_hours: 19,
-        average_grade: 90,
-    });
-
-    let undergraduate3 = Box::new(Undergraduate {
-        first_name: String::from("Brianna"),
-        last_name: String::from("Ray"),
-        student_id: 2243439,
-        major: String::from("Computer Science"),
-        year: String::from("Sophomore"),
-        credit_hours: 22,
-        average_grade: 78,
-    });
-
-    let graduate1 = Box::new(Graduate {
-        first_name: String::from("Henley"),
-        last_name: String::from("Atkins"),
-        student_id: 2234567,
-        major: String::from("Biotechnology"),
-        thesis_topic: String::from("Gene Editing"),
-        average_grade: 82,
-        research_hours: 10,
-    });
-
-    let graduate2 = Box::new(Graduate {
-        first_name: String::from("Idris"),
-        last_name: String::from("Nichols"),
-        student_id: 2236597,
-        major: String::from("Mechanical Engineering"),
-        thesis_topic: String::from("Renewable Energy"),
-        average_grade: 90,
-        research_hours: 15,
-    });
-
-    // add students to the logbook
-    log_book.add_student(undergraduate1);
-    log_book.add_student(undergraduate2);
-    log_book.add_student(undergraduate3);
-    log_book.add_student(graduate1);
-    log_book.add_student(graduate2);
-
-    // display the information of all the students
-    println!("Student Log Book Information");
-    println!("============================");
-    log_book.display_all_students();
-}
-
-// traits of students (interfaces in java)
+// traits defined for students (interface in java)
 trait Student {
     fn get_first_name(&self) -> &str;
     fn get_last_name(&self) -> &str;
@@ -77,7 +11,7 @@ trait Student {
     fn display_info(&self);
 }
 
-// undergraduate structure (object in java)
+// structure to represent an undergraduate student (object in java)
 struct Undergraduate {
     first_name: String,
     last_name: String,
@@ -88,7 +22,7 @@ struct Undergraduate {
     average_grade: f32,
 }
 
-// implementing traits to undergraduate structure (interface implementation in java)
+// implementation of the student trait for undergraduate students (interface implementation in java)
 impl Student for Undergraduate {
     fn get_first_name(&self) -> &str {
         &self.first_name
@@ -106,7 +40,7 @@ impl Student for Undergraduate {
         self.average_grade
     }
 
-    fn get_performance_status(&self) -> f64 {
+    fn get_performance_status(&self) -> String {
         if self.average_grade > 85.0 {
             "Excellent Performance".to_string()
         } else if self.average_grade > 75.0 {
@@ -117,22 +51,26 @@ impl Student for Undergraduate {
     }
 
     fn compute_tuition(&self) -> f32 {
-        let rate_per_credit = 300.0;
+        let rate_per_credit = 2000.0;
         let tuition = rate_per_credit * self.credit_hours as f32;
         tuition
     }
 
     fn display_info(&self) {
         println!(
-            "Graduate Student: {}, {} (ID: {}) - Major: {} - Year: {} - Credit Hours: {}\
-            - Average Grade: {:.2}",
+            "Graduate Student: {}, {} \n\
+            ID Number: {} \n\
+            Major: {} \n\
+            Year: {} \n\
+            Credit Hours: {} \n\
+            Average Grade: {:.2}",
             self.last_name, self.first_name, self.student_id,
             self.major, self.year, self.credit_hours, self.average_grade
         );
     }
 }
 
-// graduate structure (object in java)
+// structure to represent a graduate student (object in java)
 struct Graduate {
     first_name: String,
     last_name: String,
@@ -144,7 +82,7 @@ struct Graduate {
 }
 
 
-// implementing traits to graduate structure (interface implementation in java)
+// implementation of the student trait for graduate students (interface implementation in java)
 impl Student for Graduate {
     fn get_first_name(&self) -> &str {
         &self.first_name
@@ -162,7 +100,7 @@ impl Student for Graduate {
         self.average_grade
     }
 
-    fn get_performance_status(&self) -> f64 {
+    fn get_performance_status(&self) -> String {
         if self.average_grade > 85.0 {
             "Excellent Research".to_string()
         } else if self.average_grade > 75.0 {
@@ -173,50 +111,128 @@ impl Student for Graduate {
     }
 
     fn compute_tuition(&self) -> f32 {
-        let base_tuition = 4000.0;
-        let research_fee = 150.0 * self.research_hours as f32;
+        let base_tuition = 12450.0;
+        let research_fee = 1500.0 * self.research_hours as f32;
         let tuition = base_tuition + research_fee;
         tuition
     }
 
     fn display_info(&self) {
         println!(
-            "Graduate Student: {}, {} (ID: {}) - Major: {} - Thesis Topic: {} - Avg. Grade: {:.2}\
-            - Research Hours: {}",
+            "Graduate Student: {}, {} \n\
+            ID Number: {} \n\
+            Major: {} \n\
+            Thesis Topic: {} \n\
+            Avg. Grade: {:.2} \n\
+            Research Hours: {}",
             self.last_name, self.first_name, self.student_id, self.major,
             self.thesis_topic, self.average_grade, self.research_hours
         );
     }
 }
 
+// structure to hold the information of various students
 struct LogBook {
-    students: HashMap<u32, Box<dyn Student>>,
+    students: IndexMap<u32, Box<dyn Student>>,
 }
 
 impl LogBook {
     fn new() -> Self {
         LogBook {
-            students: HashMap::new(),
+            students: IndexMap::new(), // Initialize an empty IndexMap
         }
     }
 
+    // add student to the IndexMap
     fn add_student(&mut self, student: Box<dyn Student>) {
         self.students.insert(student.get_student_id(), student);
     }
 
+    // retrieve a student based on their ID number
     fn get_student(&self, student_id: u32) -> Option<&Box<dyn Student>> {
         self.students.get(&student_id)
     }
 
+    // retrieve a mutable reference to a student
     fn get_student_mut(&mut self, student_id: u32) -> Option<&mut Box<dyn Student>> {
         self.students.get_mut(&student_id)
     }
 
+    // display all students in the log book
     fn display_all_students(&self) {
         for student in self.students.values() {
             student.display_info();
+            println!("Performance Status: {}", student.get_performance_status());
             println!("Tuition Fee: P{:.2}", student.compute_tuition());
-            println!("---")
+            println!("-------------------------------------------------------------------")
         }
     }
+}
+
+pub(crate) fn main() {
+    // instantiate logbook
+    let mut log_book = LogBook::new();
+
+    // creation of students
+    let undergraduate1 = Box::new(Undergraduate {
+        first_name: String::from("Stephen"),
+        last_name: String::from("Coloma"),
+        student_id: 2234532,
+        major: String::from("Computer Science"),
+        year: String::from("Junior"),
+        credit_hours: 19,
+        average_grade: 93.5,
+    });
+
+    let undergraduate2 = Box::new(Undergraduate {
+        first_name: String::from("Mario"),
+        last_name: String::from("Nonato"),
+        student_id: 2230683,
+        major: String::from("Computer Science"),
+        year: String::from("Junior"),
+        credit_hours: 19,
+        average_grade: 90f32,
+    });
+
+    let undergraduate3 = Box::new(Undergraduate {
+        first_name: String::from("Brianna"),
+        last_name: String::from("Ray"),
+        student_id: 2243439,
+        major: String::from("Computer Science"),
+        year: String::from("Sophomore"),
+        credit_hours: 22,
+        average_grade: 78f32,
+    });
+
+    let graduate1 = Box::new(Graduate {
+        first_name: String::from("Henley"),
+        last_name: String::from("Atkins"),
+        student_id: 2234567,
+        major: String::from("Biotechnology"),
+        thesis_topic: String::from("Gene Editing"),
+        average_grade: 82f32,
+        research_hours: 10,
+    });
+
+    let graduate2 = Box::new(Graduate {
+        first_name: String::from("Jude"),
+        last_name: String::from("Novak"),
+        student_id: 2236597,
+        major: String::from("Mechanical Engineering"),
+        thesis_topic: String::from("Renewable Energy"),
+        average_grade: 90f32,
+        research_hours: 15,
+    });
+
+    // add students to the logbook
+    log_book.add_student(undergraduate1);
+    log_book.add_student(undergraduate2);
+    log_book.add_student(undergraduate3);
+    log_book.add_student(graduate1);
+    log_book.add_student(graduate2);
+
+    // display the information of all the students
+    println!("                 Student Log Book Information");
+    println!("===================================================================");
+    log_book.display_all_students();
 }
