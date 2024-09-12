7adcd888b8033c0824fa82265383d10bbe7c4fd3
Stephen Coloma <101116856+Stephen-Coloma@users.noreply.github.com>
2024-09-12 16:41:58 +0800

    Update main.rs
    uncommented others'code to prevent merge conflicts

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ mod shadowing;
 mod ownership;
 
 fn main() {
-    //this is our main entry point for each section. Make sure to have sub_method in your rust file guys
-    //basics::sub_method();
-    //conditionals_compound_types::sub_method();
-    //functions_control_flow::sub_method();
-    //ownership::sub_method();
-    //borrowing::sub_method();
-   // shadowing::sub_method();
+    this is our main entry point for each section. Make sure to have sub_method in your rust file guys
+    basics::sub_method();
+    conditionals_compound_types::sub_method();
+    functions_control_flow::sub_method();
+    ownership::sub_method();
+    borrowing::sub_method();
+    shadowing::sub_method();
     inheritance_and_encapsulation::sub_method();
-   // abstraction_and_polymorphism::sub_method();
+    abstraction_and_polymorphism::sub_method();
 }
