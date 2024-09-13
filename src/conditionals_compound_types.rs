a6c46f9ab9c84840a0150822d79251951d1e14e5
Stephen Coloma <2232847@slu.edu.ph>
2024-09-13 21:16:20 +0800

    add: latest on conditionals

--- src/conditionals_compound_types.rs
+++ src/conditionals_compound_types.rs
@@ -1,13 +1,28 @@
 pub fn sub_method(){
+    println!("\n\n CONDITIONAL STATEMENTS");
+    println!("---------------------------------------------------------");
     conditionals();
+    println!("---------------------------------------------------------\n\n");
+
+    println!("TUPLES");
+    println!("---------------------------------------------------------");
     tuple_compound_data_type();
+    println!("---------------------------------------------------------\n\n");
+
+    println!("ARRAYS");
+    println!("---------------------------------------------------------");
     arrays_compound_data_type();
+    println!("---------------------------------------------------------\n\n");
+
+    println!("VECTORS");
+    println!("---------------------------------------------------------");
     vectors();
+    println!("---------------------------------------------------------\n\n");
 }
 
 
 fn conditionals(){
-    /*Assuming that we already have the grade, we want to know what latin honors does it fall using decision flor*/
+    /*Scenario: Compute for Latin Honors for a certain GWA*/
     const CUM_LAUDE: f64 = 87.0;
     const MAGNA_CUM_LAUDE: f64 = 91.0;
     const SUMMA_CUM_LAUDE: f64 = 94.0;
@@ -16,16 +31,16 @@ fn conditionals(){
 
     // If-else statements
     if gwa >= SUMMA_CUM_LAUDE {
-        println!("With GWA of: {:.4}, your Latin honor is SUMMA CUM LAUDE", gwa);
+        println!("With GWA of: {}, your Latin honor is SUMMA CUM LAUDE", gwa);
     } else if gwa >= MAGNA_CUM_LAUDE {
-        println!("With GWA of: {:.3}, your Latin honor is MAGNA CUM LAUDE", gwa);
+        println!("With GWA of: {}, your Latin honor is MAGNA CUM LAUDE", gwa);
     } else if gwa >= CUM_LAUDE {
-        println!("With GWA of: {:.2}, your Latin honor is CUM LAUDE", gwa);
+        println!("With GWA of: {}, your Latin honor is CUM LAUDE", gwa);
     } else {
-        println!("With GWA of: {:.1}, you do not qualify for Latin honors", gwa);
+        println!("With GWA of: {}, you do not qualify for Latin honors", gwa);
     }
 
-    //another way is using match -- similar to switch case in java
+    //Match conditional statement -- similar to switch case in java
     let year_level: i8 = 3;
 
     match year_level {
@@ -41,35 +56,26 @@ fn tuple_compound_data_type(){
     //a tuple is a compound data type that can store elements having different types
     let student_tuple: (&str, i8, f64) = ("Stephen Coloma", 3, 89.9);
 
-    //printing the content of the tuple
-    println!("{:?}", student_tuple);
+    // Tuples are not iterable, meaning we can't use looping statements
 
-    // NOTE! tuples are not iterable
-
-    println!("---------------------------------------\n");
-
-    //ways to access the content of the tuple
+    //ways to access the element of the tuple
     println!("Name: {}",student_tuple.0);
     println!("Year Level: {}",student_tuple.1);
     println!("Current GWA: {}",student_tuple.2);
-
-    println!("---------------------------------------\n");
-    //another cool way to destructure
-    let (name, year_level, current_gwa) = student_tuple;
-    println!("Name: {}", name);
-    println!("Year Level: {}", year_level);
-    println!("Current GWA: {}", current_gwa);
 }
 
 fn arrays_compound_data_type(){
-    // arrays are iterable using the .iter() but loops will be discussed in a while
-    let grades: [f64; 5] = [85.5, 90.0, 78.5, 88.0, 92.3]; // Array of 5 grades
+    // arrays in rust are fixed length and stores the same types
+    /*Scenario: Compute average grade stored in arrays*/
+    let grades: [f64; 5] = [85.5, 90.0, 78.5, 88.0, 92.3];
     let total: f64 = grades.iter().sum();
     let average = total / grades.len() as f64;
 
     println!("Average Grade: {}", average);
 
+    //bracket symbol same with java
     let first_grade: f64 = grades[0];
+    println!("First Grade: {}", first_grade);
 }
 
 fn vectors(){
@@ -78,15 +84,14 @@ fn vectors(){
     // core methods of vectors
     student_vector.push("Stephen Coloma");
     student_vector.push("Leonhard Leung");
-    student_vector.push("Marius Nonato");
     student_vector.push("Jerwin Kyle Ramos");
 
     println!("{:?}\n", student_vector);
 
-    student_vector.pop(); //removes Jerwin Kyle Ramos
+    student_vector.pop(); //removes last element
     println!("{:?}\n", student_vector);
 
-    student_vector.remove(0); // removes Stephen Coloma
+    student_vector.remove(0); // removes index 0
     println!("{:?}\n", student_vector);
 
     student_vector.clear(); //removes all elements
